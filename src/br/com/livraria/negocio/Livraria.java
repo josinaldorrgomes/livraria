2c65e5e5208c0f7d397b984977d5bb75d961a870
josinaldorrgomes <josinaldorobertorocha@gmail.com>
2020-07-28 11:45:16 -0300

    Classes de negócio implementadas

--- src/br/com/livraria/negocio/Livraria.java
+++ src/br/com/livraria/negocio/Livraria.java
@@ -1,6 +1,8 @@
 package br.com.livraria.negocio;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class Livraria {
@@ -31,6 +33,55 @@ public class Livraria {
 		livro.setQuantidade(10);
 		livro.setPreco(110.9);
 		estoqueLivros.add(livro);
+	}
+	
+	public List<Livro> getLivros() {
+		return Collections.unmodifiableList(estoqueLivros);
+	}
+	
+	public Livro getLivro(String idLivro)  throws LivroNaoEncontradoException {
+		Livro livroProcurado = null;
+		for (Livro book : estoqueLivros) {
+			if (book.getIdLivro().equals(idLivro)) {
+				livroProcurado = book;
+			}
+		}
+		if (livroProcurado == null) {
+			throw new LivroNaoEncontradoException("Não foi possível encontrar o livro: " + idLivro);
+		}
+		return livroProcurado;
+	}
+	
+	public void comprarLivros(CarrinhoCompras carrinho) throws CompraException {
+		List<ItemCompra> itens = carrinho.getItens();
+		Iterator<ItemCompra> i = itens.iterator();
+		while (i.hasNext()) {
+			ItemCompra item = i.next();
+			Livro livro = item.getItem();
+			String id = livro.getIdLivro();
+			int quantidade = item.getQuantidade();
+			comprarLivro(id, quantidade);
+			
+		}
+	}
+	
+	public void comprarLivro(String idLivro, int qtdComprada) throws CompraException {
+		Livro livroSelecionado;
+		try {
+			livroSelecionado = getLivro(idLivro);
+		} catch (LivroNaoEncontradoException e) {
+			throw new CompraException(e.getMessage());
+		}
+		int qtdEstoque = livroSelecionado.getQuantidade();
+		if ((qtdEstoque - qtdComprada) >= 0) {
+			int novaQtd = qtdEstoque - qtdComprada;
+			livroSelecionado.setQuantidade(novaQtd);
+		} else {
+			throw new CompraException("Livro " + idLivro + "sem estoque sifuciente");
+		}
+	}
+
+	public void fechar() {
 	}
 	
 }
---
+++ src/br/com/livraria/negocio/LivrariaContextListener.java
@@ -0,0 +1,31 @@
+package br.com.livraria.negocio;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class LivrariaContextListener implements ServletContextListener {
+
+	public static final String SISTEMA_LIVRARIA = "sistemaLivraria";
+
+	public void contextInitialized(ServletContextEvent event) {
+		ServletContext context = event.getServletContext();
+		try {
+			Livraria livraria = new Livraria();
+			context.setAttribute(SISTEMA_LIVRARIA, livraria);
+		} catch (Exception e) {
+			System.out.println("O sistema de livraria não pode ser publicado no contexto: " + e.getMessage());
+		}
+
+	}
+	
+	public void contextDestroyed(ServletContextEvent event) {
+		ServletContext context = event.getServletContext();
+		Livraria livraria = (Livraria) context.getAttribute(SISTEMA_LIVRARIA);
+		if (livraria != null) {
+			livraria.fechar();
+		}
+		context.removeAttribute(SISTEMA_LIVRARIA);
+	}
+
+}
